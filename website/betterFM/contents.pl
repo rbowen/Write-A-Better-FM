25370cee83b655d4ec8b39c43f398ab15fb1bfb3
Rich Bowen <rbowen@rcbowen.com>
2010-05-14 16:51:21 -0400

    Yeah, I know, putting generated files in revision control is grounds for excommunication. So sue me.

---
+++ website/betterFM/contents.pl
@@ -0,0 +1,118 @@
+# LaTeX2HTML 2008 (1.71)
+# Associate contents original text with physical files.
+
+
+$key = q/1 0 11 3 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0/;
+$toc_section_info{$key} = '3%:%'."$dir".q|node25.html%:%Encourage customers to upgrade| unless ($toc_section_info{$key}); 
+$noresave{$key} = "$nosave";
+
+$key = q/1 0 10 3 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0/;
+$toc_section_info{$key} = '3%:%'."$dir".q|node19.html%:%IRC| unless ($toc_section_info{$key}); 
+$noresave{$key} = "$nosave";
+
+$key = q/1 0 8 3 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0/;
+$toc_section_info{$key} = '3%:%'."$dir".q|node12.html%:%FAQs| unless ($toc_section_info{$key}); 
+$noresave{$key} = "$nosave";
+
+$key = q/1 0 10 2 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0/;
+$toc_section_info{$key} = '3%:%'."$dir".q|node18.html%:%Users| unless ($toc_section_info{$key}); 
+$noresave{$key} = "$nosave";
+
+$key = q/1 0 8 4 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0/;
+$toc_section_info{$key} = '3%:%'."$dir".q|node13.html%:%Examples| unless ($toc_section_info{$key}); 
+$noresave{$key} = "$nosave";
+
+$key = q/1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0/;
+$toc_section_info{$key} = '0%:%'."$dir".q|betterFM.html%:%Write A Better FM| unless ($toc_section_info{$key}); 
+$noresave{$key} = "$nosave";
+
+$key = q/1 0 11 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0/;
+$toc_section_info{$key} = '3%:%'."$dir".q|node23.html%:%Anticipate misconceptions, and gently correct| unless ($toc_section_info{$key}); 
+$noresave{$key} = "$nosave";
+
+$key = q/1 0 11 2 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0/;
+$toc_section_info{$key} = '3%:%'."$dir".q|node24.html%:%Strong leadership from an opinionated writer| unless ($toc_section_info{$key}); 
+$noresave{$key} = "$nosave";
+
+$key = q/1 0 3 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0/;
+$toc_section_info{$key} = '2%:%'."$dir".q|node4.html%:%Define your Audience(s)| unless ($toc_section_info{$key}); 
+$noresave{$key} = "$nosave";
+
+$key = q/1 0 8 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0/;
+$toc_section_info{$key} = '2%:%'."$dir".q|node9.html%:%Document Organization| unless ($toc_section_info{$key}); 
+$noresave{$key} = "$nosave";
+
+$key = q/1 0 10 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0/;
+$toc_section_info{$key} = '2%:%'."$dir".q|node16.html%:%Involving the Community| unless ($toc_section_info{$key}); 
+$noresave{$key} = "$nosave";
+
+$key = q/1 0 11 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0/;
+$toc_section_info{$key} = '2%:%'."$dir".q|node22.html%:%Maxims| unless ($toc_section_info{$key}); 
+$noresave{$key} = "$nosave";
+
+$key = q/1 0 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0/;
+$toc_section_info{$key} = '2%:%'."$dir".q|node1.html%:%Contents| unless ($toc_section_info{$key}); 
+$noresave{$key} = "$nosave";
+
+$key = q/1 0 10 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0/;
+$toc_section_info{$key} = '3%:%'."$dir".q|node17.html%:%Developers| unless ($toc_section_info{$key}); 
+$noresave{$key} = "$nosave";
+
+$key = q/1 0 8 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0/;
+$toc_section_info{$key} = '3%:%'."$dir".q|node10.html%:%Tutorials| unless ($toc_section_info{$key}); 
+$noresave{$key} = "$nosave";
+
+$key = q/1 0 4 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0/;
+$toc_section_info{$key} = '2%:%'."$dir".q|node5.html%:%Print vs. Online Documentation| unless ($toc_section_info{$key}); 
+$noresave{$key} = "$nosave";
+
+$key = q/1 0 10 4 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0/;
+$toc_section_info{$key} = '3%:%'."$dir".q|node20.html%:%Mailing Lists| unless ($toc_section_info{$key}); 
+$noresave{$key} = "$nosave";
+
+$key = q/1 0 5 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0/;
+$toc_section_info{$key} = '2%:%'."$dir".q|node6.html%:%Listening to the Customer| unless ($toc_section_info{$key}); 
+$noresave{$key} = "$nosave";
+
+$key = q/1 0 8 2 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0/;
+$toc_section_info{$key} = '3%:%'."$dir".q|node11.html%:%The Manual| unless ($toc_section_info{$key}); 
+$noresave{$key} = "$nosave";
+
+$key = q/1 0 6 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0/;
+$toc_section_info{$key} = '2%:%'."$dir".q|node7.html%:%Handling Translation| unless ($toc_section_info{$key}); 
+$noresave{$key} = "$nosave";
+
+$key = q/1 0 13 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0/;
+$toc_section_info{$key} = '2%:%'."$dir".q|node27.html%:%About this document ...%:%<tex2html_star_mark>| unless ($toc_section_info{$key}); 
+$noresave{$key} = "$nosave";
+
+$key = q/1 0 10 5 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0/;
+$toc_section_info{$key} = '3%:%'."$dir".q|node21.html%:%Making People Welcome| unless ($toc_section_info{$key}); 
+$noresave{$key} = "$nosave";
+
+$key = q/1 0 2 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0/;
+$toc_section_info{$key} = '2%:%'."$dir".q|node2.html%:%RTFM| unless ($toc_section_info{$key}); 
+$noresave{$key} = "$nosave";
+
+$key = q/1 0 2 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0/;
+$toc_section_info{$key} = '3%:%'."$dir".q|node3.html%:%Technical mumbo-jumbo| unless ($toc_section_info{$key}); 
+$noresave{$key} = "$nosave";
+
+$key = q/1 0 12 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0/;
+$toc_section_info{$key} = '2%:%'."$dir".q|node26.html%:%Index| unless ($toc_section_info{$key}); 
+$noresave{$key} = "$nosave";
+
+$key = q/1 0 9 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0/;
+$toc_section_info{$key} = '2%:%'."$dir".q|node14.html%:%Documentation Formats| unless ($toc_section_info{$key}); 
+$noresave{$key} = "$nosave";
+
+$key = q/1 0 7 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0/;
+$toc_section_info{$key} = '2%:%'."$dir".q|node8.html%:%Freqently Answered Questions| unless ($toc_section_info{$key}); 
+$noresave{$key} = "$nosave";
+
+$key = q/1 0 9 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0/;
+$toc_section_info{$key} = '3%:%'."$dir".q|node15.html%:%Choosing a source format| unless ($toc_section_info{$key}); 
+$noresave{$key} = "$nosave";
+
+1;
+
---
+++ website/betterFM/sections.pl
@@ -0,0 +1,146 @@
+# LaTeX2HTML 2008 (1.71)
+# Associate sections original text with physical files.
+
+
+$key = q/1 0 11 3 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0/;
+$section_info{$key} = '3%:%'."$dir".q|node25.html%:%Encourage customers to upgrade%:%| unless ($section_info{$key}); 
+$noresave{$key} = "$nosave";
+$done{"${dir}node25.html"} = 1;
+
+$key = q/1 0 10 3 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0/;
+$section_info{$key} = '3%:%'."$dir".q|node19.html%:%IRC%:%| unless ($section_info{$key}); 
+$noresave{$key} = "$nosave";
+$done{"${dir}node19.html"} = 1;
+
+$key = q/1 0 8 3 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0/;
+$section_info{$key} = '3%:%'."$dir".q|node12.html%:%FAQs%:%| unless ($section_info{$key}); 
+$noresave{$key} = "$nosave";
+$done{"${dir}node12.html"} = 1;
+
+$key = q/1 0 10 2 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0/;
+$section_info{$key} = '3%:%'."$dir".q|node18.html%:%Users%:%| unless ($section_info{$key}); 
+$noresave{$key} = "$nosave";
+$done{"${dir}node18.html"} = 1;
+
+$key = q/1 0 8 4 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0/;
+$section_info{$key} = '3%:%'."$dir".q|node13.html%:%Examples%:%| unless ($section_info{$key}); 
+$noresave{$key} = "$nosave";
+$done{"${dir}node13.html"} = 1;
+
+$key = q/1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0/;
+$section_info{$key} = '0%:%'."$dir".q|betterFM.html%:%Write A Better FM%:%| unless ($section_info{$key}); 
+$noresave{$key} = "$nosave";
+$done{"${dir}betterFM.html"} = 1;
+
+$key = q/1 0 11 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0/;
+$section_info{$key} = '3%:%'."$dir".q|node23.html%:%Anticipate misconceptions, and gently correct%:%| unless ($section_info{$key}); 
+$noresave{$key} = "$nosave";
+$done{"${dir}node23.html"} = 1;
+
+$key = q/1 0 11 2 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0/;
+$section_info{$key} = '3%:%'."$dir".q|node24.html%:%Strong leadership from an opinionated writer%:%| unless ($section_info{$key}); 
+$noresave{$key} = "$nosave";
+$done{"${dir}node24.html"} = 1;
+
+$key = q/1 0 3 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0/;
+$section_info{$key} = '2%:%'."$dir".q|node4.html%:%Define your Audience(s)%:%| unless ($section_info{$key}); 
+$noresave{$key} = "$nosave";
+$done{"${dir}node4.html"} = 1;
+
+$key = q/1 0 8 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0/;
+$section_info{$key} = '2%:%'."$dir".q|node9.html%:%Document Organization%:%| unless ($section_info{$key}); 
+$noresave{$key} = "$nosave";
+$done{"${dir}node9.html"} = 1;
+
+$key = q/1 0 10 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0/;
+$section_info{$key} = '2%:%'."$dir".q|node16.html%:%Involving the Community%:%| unless ($section_info{$key}); 
+$noresave{$key} = "$nosave";
+$done{"${dir}node16.html"} = 1;
+
+$key = q/1 0 11 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0/;
+$section_info{$key} = '2%:%'."$dir".q|node22.html%:%Maxims%:%| unless ($section_info{$key}); 
+$noresave{$key} = "$nosave";
+$done{"${dir}node22.html"} = 1;
+
+$key = q/1 0 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0/;
+$section_info{$key} = '2%:%'."$dir".q|node1.html%:%Contents%:%| unless ($section_info{$key}); 
+$noresave{$key} = "$nosave";
+$done{"${dir}node1.html"} = 1;
+
+$key = q/1 0 10 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0/;
+$section_info{$key} = '3%:%'."$dir".q|node17.html%:%Developers%:%| unless ($section_info{$key}); 
+$noresave{$key} = "$nosave";
+$done{"${dir}node17.html"} = 1;
+
+$key = q/1 0 8 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0/;
+$section_info{$key} = '3%:%'."$dir".q|node10.html%:%Tutorials%:%| unless ($section_info{$key}); 
+$noresave{$key} = "$nosave";
+$done{"${dir}node10.html"} = 1;
+
+$key = q/1 0 4 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0/;
+$section_info{$key} = '2%:%'."$dir".q|node5.html%:%Print vs. Online Documentation%:%| unless ($section_info{$key}); 
+$noresave{$key} = "$nosave";
+$done{"${dir}node5.html"} = 1;
+
+$key = q/1 0 10 4 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0/;
+$section_info{$key} = '3%:%'."$dir".q|node20.html%:%Mailing Lists%:%| unless ($section_info{$key}); 
+$noresave{$key} = "$nosave";
+$done{"${dir}node20.html"} = 1;
+
+$key = q/1 0 5 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0/;
+$section_info{$key} = '2%:%'."$dir".q|node6.html%:%Listening to the Customer%:%| unless ($section_info{$key}); 
+$noresave{$key} = "$nosave";
+$done{"${dir}node6.html"} = 1;
+
+$key = q/1 0 8 2 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0/;
+$section_info{$key} = '3%:%'."$dir".q|node11.html%:%The Manual%:%| unless ($section_info{$key}); 
+$noresave{$key} = "$nosave";
+$done{"${dir}node11.html"} = 1;
+
+$key = q/1 0 6 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0/;
+$section_info{$key} = '2%:%'."$dir".q|node7.html%:%Handling Translation%:%| unless ($section_info{$key}); 
+$noresave{$key} = "$nosave";
+$done{"${dir}node7.html"} = 1;
+
+$key = q/1 0 13 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0/;
+$section_info{$key} = '2%:%'."$dir".q|node27.html%:%About this document ...%:%| unless ($section_info{$key}); 
+$noresave{$key} = "$nosave";
+$done{"${dir}node27.html"} = 1;
+
+$key = q/1 0 10 5 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0/;
+$section_info{$key} = '3%:%'."$dir".q|node21.html%:%Making People Welcome%:%| unless ($section_info{$key}); 
+$noresave{$key} = "$nosave";
+$done{"${dir}node21.html"} = 1;
+
+$key = q/1 0 2 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0/;
+$section_info{$key} = '2%:%'."$dir".q|node2.html%:%RTFM%:%| unless ($section_info{$key}); 
+$noresave{$key} = "$nosave";
+$done{"${dir}node2.html"} = 1;
+
+$key = q/1 0 2 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0/;
+$section_info{$key} = '3%:%'."$dir".q|node3.html%:%Technical mumbo-jumbo%:%| unless ($section_info{$key}); 
+$noresave{$key} = "$nosave";
+$done{"${dir}node3.html"} = 1;
+
+$key = q/1 0 12 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0/;
+$section_info{$key} = '2%:%'."$dir".q|node26.html%:%Index%:%| unless ($section_info{$key}); 
+$noresave{$key} = "$nosave";
+$done{"${dir}node26.html"} = 1;
+
+$key = q/1 0 9 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0/;
+$section_info{$key} = '2%:%'."$dir".q|node14.html%:%Documentation Formats%:%| unless ($section_info{$key}); 
+$noresave{$key} = "$nosave";
+$done{"${dir}node14.html"} = 1;
+
+$key = q/1 0 7 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0/;
+$section_info{$key} = '2%:%'."$dir".q|node8.html%:%Freqently Answered Questions%:%| unless ($section_info{$key}); 
+$noresave{$key} = "$nosave";
+$done{"${dir}node8.html"} = 1;
+
+$key = q/1 0 9 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0/;
+$section_info{$key} = '3%:%'."$dir".q|node15.html%:%Choosing a source format%:%| unless ($section_info{$key}); 
+$noresave{$key} = "$nosave";
+$done{"${dir}node15.html"} = 1;
+
+1;
+
